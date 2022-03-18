677be0d1641a988bf6653d12a4f653f88bd520eb
Micael Nussbaumer <micaelnussbaumer@gmail.com>
2022-03-18 19:48:34 +0000

    Add validation for configuration options and warn if an option is passed that isn't recognized - might mean it's a typo

--- lib/configuration/flagsmith_configuration.ex
+++ lib/configuration/flagsmith_configuration.ex
@@ -1,4 +1,7 @@
 defmodule Flagsmith.Configuration do
+  use TypedEctoSchema
+  require Logger
+
   @default_url "https://api.flagsmith.com/api/v1"
   @environment_header "X-Environment-Key"
 
@@ -9,6 +12,8 @@ defmodule Flagsmith.Configuration do
     analytics: "/analytics/flags/",
     environment: "/environment-document/"
   }
+
+  @min_refresh_interval if(Mix.env() == :test, do: 1, else: 60_000)
 
   @enforce_keys [:environment_key]
   defstruct [
@@ -67,6 +72,79 @@ defmodule Flagsmith.Configuration do
       Enum.reduce(@default_keys, config, fn key, config_acc ->
         maybe_add_key(config_acc, key, opts)
       end)
+      |> validate!()
+      |> warn_if_unknown_opts(opts)
+    end
+  end
+
+  defp validate!(%__MODULE__{} = config) do
+    Enum.all?([:environment_key | @default_keys], fn k ->
+      value = Map.fetch!(config, k)
+
+      case validate(k, value) do
+        true -> true
+        {:error, msg} -> raise "#{k} #{msg}"
+      end
+    end)
+
+    config
+  end
+
+  defp warn_if_unknown_opts(config, opts) do
+    Enum.map(opts, fn {k, _} ->
+      case is_map_key(config, k) do
+        true ->
+          :ok
+
+        false ->
+          Logger.warn("unknown option #{inspect(k)} passed as configuration to Flagsmith.Client")
+      end
+    end)
+
+    config
+  end
+
+  defp validate(key, value) do
+    case key do
+      :environment_key ->
+        is_binary(value) || {:error, "needs to be a String.t()"}
+
+      :api_url ->
+        is_binary(value) || {:error, "needs to be a String.t()"}
+
+      :default_flag_handler ->
+        is_function(value, 1) or is_nil(value) ||
+          {:error, "needs to be a 1 arity function or nil"}
+
+      :custom_headers ->
+        (is_list(value) and
+           Enum.all?(value, fn pair ->
+             with true <- is_tuple(pair) and tuple_size(pair) == 2,
+                  {k, v} <- pair do
+               is_binary(k) and is_binary(v)
+             else
+               _ -> false
+             end
+           end)) ||
+          {:error, "needs to be a list of 2 sized tuples, with both elements String.t()"}
+
+      :request_timeout_milliseconds ->
+        (is_integer(value) and value >= 5000) ||
+          {:error, "needs to be an integer equal or bigger than 5000"}
+
+      :enable_local_evaluation ->
+        is_boolean(value) || {:error, "needs to be true or false"}
+
+      :environment_refresh_interval_milliseconds ->
+        (is_integer(value) and value >= @min_refresh_interval) ||
+          {:error, "needs to be an integer equal or bigger than #{@min_refresh_interval}"}
+
+      :retries ->
+        (is_integer(value) and value >= 0) ||
+          {:error, "needs to be an integer equal or bigger than 0"}
+
+      :enable_analytics ->
+        is_boolean(value) || {:error, "needs to be true or false"}
     end
   end
 
