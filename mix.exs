6140542d86353c5f5b5358f09399fd8aabb871f1
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-06-15 11:01:49 +0100

    QoL updates (#30)
    * Bump version 2.0.0
    
    * qol updates

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FlagsmithEngine.MixProject do
   def project do
     [
       app: :flagsmith_engine,
-      version: "1.1.1",
+      version: "2.0.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
